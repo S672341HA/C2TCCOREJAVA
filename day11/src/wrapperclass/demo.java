bc232b536284d7d9bdde9d1e32ed3d4c6a610cc3
user <user@DESKTOP-IET0UHC>
2022-06-22 22:41:00 +0530

    firstcommit

---
+++ day11/src/wrapperclass/demo.java
@@ -0,0 +1,16 @@
+package wrapperclass;
+
+public class demo {
+
+	public static void main(String[] args) {
+		int a=20;
+		Integer i=Integer.valueOf(a);//converting int into Integer explicitly
+		Integer j=a;//autoboxing
+		System.out.println(a+" "+i+" "+j);
+
+		
+
+
+	}
+
+}
---
+++ day11/src/wrapperclass/demo1.java
@@ -0,0 +1,13 @@
+package wrapperclass;
+
+public class demo1 {
+	public static void main(String args[]) {
+		Integer a=(3);
+		int i=a.intValue();//converting Integer to int explicity
+		int j=a;
+		System.out.println(a+" "+i+" "+j);
+
+		
+	}
+
+}
