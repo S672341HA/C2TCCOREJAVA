abedcdbc4351cf8a6fd01e974c42a083ae48888a
user <user@DESKTOP-IET0UHC>
2022-07-19 06:28:11 +0530

    firstcommit

---
+++ day13/src/replace/ReplaceExample1.java
@@ -0,0 +1,16 @@
+package replace;
+
+public class ReplaceExample1 {
+ 
+
+	 
+
+		public static void main(String[] args) {
+			String s1 = "Negative thoughts";
+			String replaceString = s1.replace('e','a');
+			System.out.println(replaceString);
+
+		}
+
+	}
+
